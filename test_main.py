343468dece4b33488eb78d12fcecf9cea5716d40
Corentin <Fantacoca974@gmail.com>
2022-02-15 09:35:55 +0100

    Add gitingnore and pytest

---
+++ test_main.py
@@ -0,0 +1,17 @@
+import main
+import pytest
+
+def test_fact():
+    # À compléter...
+    pass
+
+def test_roots():
+    # À compléter...
+    pass
+
+def test_integrate():
+    # À compléter...
+    pass
+
+def test_add():
+    assert main.add(3, 4)==7
