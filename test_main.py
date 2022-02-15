011b233d238f457331897e36ae2a3f8d28514696
Corentin <Fantacoca974@gmail.com>
2022-02-15 11:19:22 +0100

    add test_fact and main fact

--- test_main.py
+++ test_main.py
@@ -17,3 +17,8 @@ def test_add():
     assert main.add(3, 4)==7
     assert main.add(3, 5)==8
     assert main.add(2, 4)==6
+
+def test_fact():
+    assert main.fact(5)==120
+    assert main.fact(3)==6
+    assert main.fact(4)==24
