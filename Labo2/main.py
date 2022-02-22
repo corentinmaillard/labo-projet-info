517e837d241b7fa77350ae07dbaa51dc700b54d9
Corentin <Fantacoca974@gmail.com>
2022-02-22 09:53:51 +0100

    add ex2

--- Labo2/main.py
+++ Labo2/main.py
@@ -17,14 +17,34 @@ print(p.match('123'))
 print(p.match('+123'))
 
 #ex 1.c
-pattern = r'\d\d{3}[A-Z]{3}|\d[A-Z]{3}\d{3}'
+pattern = r'\d?\d{3}[A-Z]{3}|\d?[A-Z]{3}\d{3}'
 p = re.compile(pattern)
 
 print(p.match('1234AFD'))
+print(p.match('234AFD'))
 print(p.match('1GHT534'))
+print(p.match('GHT534'))
 
 #ex 1.d
 pattern = r'C:\\'
 p = re.compile(pattern)
 
 print(p.match('C:\\'))
+
+#ex 2
+def read(lignes):
+    pattern = r'\d+'
+    p = re.compile(pattern)
+    refligne = 1
+    for ligne in lignes:
+        stringLine = str(refligne)
+        list = p.findall(ligne)
+        listString=""
+        for element in list:
+            listString+=str(element)+", "
+        print("Line "+stringLine+": "+listString)
+        refligne+=1
+
+filin = open("text.txt", "r")
+read(filin.readlines())
+filin.close()
